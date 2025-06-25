a3d846cf0264048f82431ef16de43203628cabdb
Onetyten <onetyten@gmail.com>
2025-06-25 20:04:22 +0100

    config disable logs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/lib/disableLogs'
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   /* config options here */
 };
 
 export default nextConfig;
+
